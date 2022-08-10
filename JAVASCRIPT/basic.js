c95916cbf278ef3fd8ea9cb59128c4b21d696d02
Zac <a19860101@gmail.com>
2022-08-10 14:17:15 +0800

    20220810

---
+++ JAVASCRIPT/basic.js
@@ -0,0 +1,21 @@
+var a = 10;
+/* 
+    1. 不可用數字開頭
+    2. 不可使用特殊符號
+    3. 不可使用保留字
+*/
+
+let b = 99;
+
+console.log(b);
+
+/* 
+    var vs let
+    1. 範疇(scope)不同
+    2. let 不可重複宣告
+*/
+
+var a = 123;
+b = b + 1;
+
+console.log(b);
