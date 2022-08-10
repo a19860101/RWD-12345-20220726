8ad1d92f8d9265fedb0dba9b731ffdbd44907e11
Zac <a19860101@gmail.com>
2022-08-10 14:53:42 +0800

    20220810

---
+++ JAVASCRIPT/operator.js
@@ -0,0 +1,28 @@
+//運算子
+
+let x = 10;
+let y = 6;
+
+// 算術運算子
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+// 比較運算子
+console.log(x > y);
+console.log(x >= y);
+console.log(x < y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x === y);
+console.log(x != y);
+
+// 賦值運算子
+console.log(x = y); // x = 6
+console.log(x += y); // x = x + y 12
+console.log(x -= y); // x = x - y 6
+console.log(x *= y); // x = x * y 36
+console.log(x /= y); // x = x / y 6
+console.log(x %= y); // x = x % y 0
