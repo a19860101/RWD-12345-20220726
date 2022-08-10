0d865e546da25edc97e80ea305491685193c9ed4
Zac <a19860101@gmail.com>
2022-08-10 16:34:22 +0800

    20220810

---
+++ JAVASCRIPT/array2.js
@@ -0,0 +1,15 @@
+const drinks = ['紅茶','綠茶','奶茶','拿鐵'];
+
+// push()
+drinks.push('美式咖啡');
+
+// pop()
+drinks.pop();
+
+// unshift()
+drinks.unshift('可樂');
+
+// shift()
+drinks.shift()
+
+console.log(drinks);
