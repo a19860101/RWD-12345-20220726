9fdb97a52a97e62dec1820929346d95e780311d2
Zac <a19860101@gmail.com>
2022-08-11 15:04:43 +0800

    20220811

--- JAVASCRIPT/object.js
+++ JAVASCRIPT/object.js
@@ -15,3 +15,36 @@ console.log(obj.mail);
 console.log(obj['id']);
 console.log(obj['name']);
 console.log(obj['mail']);
+
+const drinks = [
+    {
+        id: 1,
+        name: '阿薩姆紅茶',
+        price: 20
+    },
+    {
+        id: 2,
+        name: '茉莉綠茶',
+        price: 20,
+    },{
+        id: 3,
+        name: '珍珠奶茶',
+        price: 45,
+    },
+    {
+        id: 4,
+        name: '冰淇淋紅茶',
+        price: 55
+    }
+];
+console.log(drinks);
+
+const drink_name = drinks.map(function(data){
+    return data.name;
+})
+const drink_price = drinks.map(function(data){
+    return data.price;
+})
+
+console.log(drink_name)
+console.log(drink_price)
