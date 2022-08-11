86d1644e7dded2006da928eb2d298e580156bbbf
Zac <a19860101@gmail.com>
2022-08-11 14:56:49 +0800

    20220811

---
+++ JAVASCRIPT/object.js
@@ -0,0 +1,17 @@
+// const obj = Object();
+// obj.id = 1;
+// obj.name = 'John';
+// obj.mail = 'john1234@gmail.com'
+
+const obj = {
+    id: 1,
+    name: 'John',
+    mail: 'john333@gmail.com'
+}
+console.log(obj);
+console.log(obj.id);
+console.log(obj.name);
+console.log(obj.mail);
+console.log(obj['id']);
+console.log(obj['name']);
+console.log(obj['mail']);
