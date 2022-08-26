dd97808b8093198b714e626ddb2ac7bf9f0b861d
Zac <a19860101@gmail.com>
2022-08-26 16:59:36 +0800

    20220826

--- TAILWIND/tailwind.config.js
+++ TAILWIND/tailwind.config.js
@@ -1,9 +1,16 @@
 tailwind.config = {
     theme: {
+        fontFamily: {
+            sans: ['Gill Sans MT', 'sans-serif'],
+            serif: ['georgia', 'serif'],
+        },
         extend: {
             colors:{
                 primary: 'greenyellow',
                 danger: 'red'
+            },
+            spacing: {
+                '95':'95px'
             }
         }
     }
